1500f80ec873908b035375f79abff2f39807a389
massaf-it <mahmoud.assaf@fluance.ch>
2014-08-20 14:33:29 +0200

    12

--- js/index.js
+++ js/index.js
@@ -34,6 +34,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		alert('function onDeviceReady');
+		StatusBar.overlaysWebView(false);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
